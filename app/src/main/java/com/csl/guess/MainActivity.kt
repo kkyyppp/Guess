d6769900088efe1cb63d67f84e474837ca29c973
kkyyppp <kkyyppp@gmail.com>
2020-06-09 11:10:46 +0800

    2020/06/09 單元6

--- app/src/main/java/com/csl/guess/MainActivity.kt
+++ app/src/main/java/com/csl/guess/MainActivity.kt
@@ -2,15 +2,42 @@ package com.csl.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MainActivity"
     val secretNumber = SecretNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "Secret number: $secretNumber")
 
+    }
 
+    fun checck(view: View) {
+        val n = ed_number.text.toString().toInt()
+        println("number: $n")
+        Log.d(TAG, "number: $n")
+
+        val diff = secretNumber.validate(n)
+        var msg = "Yes, you got it"
+        if (diff < 0) {
+           msg = "Bigger"
+        } else if (diff > 0){
+            msg = "smaller"
+        }
+
+       // Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(msg)
+            .setPositiveButton("ok", null)
+            .show()
     }
 }
